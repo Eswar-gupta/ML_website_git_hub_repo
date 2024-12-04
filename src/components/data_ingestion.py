d1313fa5231f7676e8beae8f94c692e6e02e0ae7
Eswar-gupta <v.eswar2005@gmail.com>
2024-12-04 23:55:52 +0530

    Vedio-5 completed

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -7,6 +7,9 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
 
+from src.components.data_tranformation import DataTransformation
+from src.components.data_tranformation import DataTransformationConfig
+
 import os
 print("Current working directory:", os.getcwd())
 
@@ -51,3 +54,6 @@ class DataIngestion:
 if __name__=="__main__":
     obj=DataIngestion()
     train_data,test_data=obj.initiate_data_ingestion()
+
+    data_transformation=DataTransformation()
+    train_arr,test_arr,_=data_transformation.initiate_data_transformation(train_data,test_data)
