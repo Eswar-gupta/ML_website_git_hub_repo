4592e0fee5a531f49a1bd2b1f1800c5a2b189c24
Eswar-gupta <156205485+Eswar-gupta@users.noreply.github.com>
2024-12-22 18:10:31 +0000

    made it as dog

--- streamlit.py
+++ streamlit.py
@@ -5,7 +5,7 @@ from src.pipelines.predict_pipeline import CustomData, PredictPipeline
 
 # Load your existing model and data preprocessor (if applicable)
 
-st.title("kali kindha Gayathri website")  # Set the app title
+st.title("Gayathri")  # Set the app title
 
 # Define input fields for user interaction
 gender = st.selectbox("Gender", ("male", "female"))
